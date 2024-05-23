1e4966094393d6871a4f436e4b245e5c6736bcbe
arvindkri-03 <arvindkriyadav03@gmail.com>
2024-05-23 12:44:15 +0530

    Modified some content In the code

--- list.js
+++ list.js
@@ -39,25 +39,3 @@ showTask();
 
 
 
-function moveSelectedItemsToEnd() {
-    // Get the list container
-    const listContainer = document.getElementById('todoList');
-
-    // Get all list items
-    const listItems = Array.from(listContainer.getElementsByTagName('LI'));
-
-    // Loop through the list items in reverse to safely remove items while iterating
-    for (let i = listItems.length - 1; i >= 0; i--) {
-        const item = listItems[i];
-        // Check if the item is selected (has the 'checked' class)
-        if (item.classList.contains('checked')) {
-            // Remove the item from its current position
-            listContainer.removeChild(item);
-            // Append the item to the end of the list
-            listContainer.appendChild(item);
-        }
-    }
-}
-
-// Example usage: Add a button click event to move selected items to end
-document.getElementById('moveButton').addEventListener('click', moveSelectedItemsToEnd);
